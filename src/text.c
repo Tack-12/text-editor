40aa727ac79cc52e6be8e225dccb202e73e8fe1a
Tack-12 <pranayauni1@gmail.com>
2026-09-18 10:12:07 -0500

    added capacity and function to add words to the line

--- src/text.c
+++ src/text.c
@@ -1,12 +1,13 @@
-
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define INI_CHARS_PER_LINE 80
 
 typedef struct {
   char *line;
   size_t size;
+  size_t capacity;
 } TextData;
 
 // Creating a dynamically allocatable data structure to store text data.
@@ -15,40 +16,47 @@ TextData *dynamic_text() {
   TextData *text = malloc(sizeof(TextData));
 
   text->line = malloc((sizeof(char)) * INI_CHARS_PER_LINE);
-  text->size = INI_CHARS_PER_LINE;
+  text->size = 0;
+  text->capacity = INI_CHARS_PER_LINE;
 
   return text;
 }
 
 // Reallocate the size of data if the text size is less than dedicated size.
-TextData *extend_line(TextData *text) {
+void extend_line(TextData *text) {
 
-  if (text->size >= INI_CHARS_PER_LINE) {
+  size_t new_cap = text->capacity * 2;
+  void *temp = realloc(text->line, new_cap);
 
-    size_t new_size = text->size * 2;
-
-    void *temp = realloc(text->line, new_size);
-
-    if (temp == NULL && text->size > 0) {
-      perror("Error Shrinking the size of the text per line");
-      return text;
-    }
-
-    text->line = temp;
-    text->size = new_size;
-    return text;
+  if (temp == NULL && text->size > 0) {
+    perror("Error Shrinking the size of the text per line");
   }
-  return text;
+
+  text->line = temp;
+  text->capacity = new_cap;
+}
+
+void enterText(TextData *text, char c) {
+
+  if (text->size >= text->capacity) {
+    extend_line(text);
+  }
+
+  if (c != '\n' && c != EOF) {
+    text->line[text->size] = c;
+    text->size++;
+  }
 }
 
 int main(void) {
 
   TextData *text = dynamic_text();
+  char c;
 
-  text->line = "Hello this is a test to see if the data structure works";
-  text->size = sizeof(text->line);
-
-  printf("%s is the line and its size is %zu", text->line, text->size);
-
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    enterText(text, c);
+  }
+  printf("%s is the line and its size is %zu ; however its capacity is %zu",
+         text->line, text->size, text->capacity);
   free(text);
 }
